d24bbd58179ec6b40146c28f20f1c5d237366a80
ewallace <ewallace@berkleyre.com.au>
2017-02-06 13:24:14 +1100

    feat() configure swagger

--- src/DungeonMasterToolsService/Startup.cs
+++ src/DungeonMasterToolsService/Startup.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DungeonMasterTools
 {
@@ -29,6 +26,11 @@ namespace DungeonMasterTools
         {
             // Add framework services.
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Dungeon Master Tools", Version = "v1" });
+            }
+        );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +40,10 @@ namespace DungeonMasterTools
             loggerFactory.AddDebug();
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUi(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dungeon Master Tools");
+            });
         }
     }
 }
